3cefabc336703359c31188c8e7e118a69b255d5e
Pansfortress <Japan8789@gmail.com>
2020-04-15 00:20:57 -0700

    Setup lecture details

---
+++ part2/lecture/src/App.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+
+const Note = ({ note }) => {
+  return <li>{note.content}</li>;
+};
+
+const App = props => {
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState("a new note");
+  const [showAll, setShowAll] = useState(false);
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important === true);
+
+  const addNote = event => {
+    event.preventDefault();
+    console.log("button clicked", event.target);
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+      id: notes.length + 1
+    };
+
+    // Concat returns a new array
+    setNotes(notes.concat(noteObject));
+    setNewNote("");
+  };
+
+  const handleNoteChange = event => {
+    setNewNote(event.target.value);
+  };
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notesToShow.map(note => (
+          <Note key={note.id} note={note} />
+        ))}
+      </ul>
+
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
+
+      <button onClick={() => setShowAll(!showAll)}>Toggle Showing</button>
+    </div>
+  );
+};
+
+export default App;
