d548329dc6ecb72fe7a477eca2bbd051f09c96ba
Pansfortress <Japan8789@gmail.com>
2020-05-10 20:26:52 -0700

    Updates

---
+++ part2/express_example/build/precache-manifest.04091ddf659754c499b60c1fdfd6a560.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3d5ea35d8de5932fcd4285540ec475a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57bc16ce090f4ba31dc9",
+    "url": "/static/js/2.ed9069d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ed9069d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb3a09902986f9a69096",
+    "url": "/static/js/main.6b59b181.chunk.js"
+  },
+  {
+    "revision": "f5d6224b05757cccb34a",
+    "url": "/static/js/runtime-main.e8d2dc15.js"
+  }
+]);
